b4673498642f904ef57bc4cd01af035389c26480
Asim Kaghzi <akaghzi@opensuse.sjrb.ad>
2013-05-27 14:36:44 -0400

    added validations to allergy and current medication models

--- app/models/allergy.rb
+++ app/models/allergy.rb
@@ -2,5 +2,6 @@ class Allergy < ActiveRecord::Base
   belongs_to :patient
   validates :substance_name, :substance_type, :reaction, presence: true
   validates :substance_type, inclusion: {in: VALID_SUBSTANCETYPE }
-  validates :substance_name, uniqueness: {case_sensitive: false}
+  validates :substance_name, uniqueness: {scope: :patient, message: "allergen can appear once for a patient", case_sensitive: false}
+  before_save { |allergy| allergy.substance_name = substance_name.downcase }
 end
--- app/models/current_medication.rb
+++ app/models/current_medication.rb
@@ -1,3 +1,7 @@
 class CurrentMedication < ActiveRecord::Base
   belongs_to :patient
+  validates :name, :dosage, :frequency, presence: true
+  validates :name, uniqueness: {scope: :patient, message: "medication can appear once for a patient", case_sensitive: false}
+  before_save { |current_medication| current_medication.name = name.downcase }
+  
 end

10a5c562818e06d5b9290a73ffd7f74aeb51deb3
akaghzi <Ilikegithub123>
2013-06-03 17:21:37 -0400

    dosage frequency model changed

--- db/migrate/20130603060654_create_medication_frequencies.rb
+++ db/migrate/20130603060654_create_medication_frequencies.rb
@@ -1,7 +1,9 @@
 class CreateMedicationFrequencies < ActiveRecord::Migration
   def change
     create_table :medication_frequencies do |t|
-      t.string :name
+      t.string :full_name
+      t.string :description
+      t.string :short_name
 
       t.timestamps
     end
--- db/migrate/20130603060721_create_medication_routes.rb
+++ db/migrate/20130603060721_create_medication_routes.rb
@@ -1,7 +1,9 @@
 class CreateMedicationRoutes < ActiveRecord::Migration
   def change
     create_table :medication_routes do |t|
-      t.string :name
+      t.string :full_name
+      t.string :description
+      t.string :short_name
 
       t.timestamps
     end

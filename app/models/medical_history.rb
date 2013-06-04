f0fc15d14b9374de45e16afad6f38fd715182ee7
Asim Kaghzi <akaghzi@gmail.com>
2013-06-04 00:11:39 -0600

    fixed few issues

--- app/models/medical_history.rb
+++ app/models/medical_history.rb
@@ -1,4 +1,6 @@
 class MedicalHistory < ActiveRecord::Base
   belongs_to :patient
   validates :disease, uniqueness: {scope: [:patient, :self_or_family], message: "can appear once for patient or the family member"}
+  # scope :self_medical_history, lambda: {"where self_or_family = 'self'"}
+  # scope :family_medical_history, lambda: {"where self_or_family <>  'self'"}
 end

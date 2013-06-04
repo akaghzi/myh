97f17fe786fdfda0e2fb36b0495ae0caf0a0448c
Asim Kaghzi <akaghzi@gmail.com>
2013-06-03 18:20:56 -0600

    moved png files from subdirs to main images dir

--- app/models/lab_test.rb
+++ app/models/lab_test.rb
@@ -1,4 +1,6 @@
 class LabTest < ActiveRecord::Base
+  validates :full_name, :short_name, presence: true
+  validates :full_name, :short_name, uniqueness: {case_sensitive: false}
   has_many :visit_lab_test_xrefs
   has_many :visits, through: :visit_lab_test_xrefs
 end

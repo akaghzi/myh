3fdd6876f02a97d41be402bcdecb8595b7b94759
akaghzi <Ilikegithub123>
2013-06-05 20:20:23 -0400

    fixed case condition for medication route to model

--- app/models/current_medication.rb
+++ app/models/current_medication.rb
@@ -3,5 +3,6 @@ class CurrentMedication < ActiveRecord::Base
   validates :name, :dosage, :frequency, presence: true
   validates :name, uniqueness: {scope: :patient, message: "medication can appear once for a patient", case_sensitive: false}
   before_save { |current_medication| current_medication.name = name.downcase }
+  before_save { |current_medication| current_medication.route = route.downcase }
   
 end

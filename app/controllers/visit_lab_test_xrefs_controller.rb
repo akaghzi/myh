96564e681a03d9299b4f0ee9cdcfdbffecab50a0
Asim Kaghzi <akaghzi@gmail.com>
2013-06-06 23:42:35 -0600

    replaced surgery scaffold with hospitalization

--- app/controllers/visit_lab_test_xrefs_controller.rb
+++ app/controllers/visit_lab_test_xrefs_controller.rb
@@ -7,7 +7,6 @@ class VisitLabTestXrefsController < ApplicationController
     # build lab_test_xrefs for the visit
     # lab_tests = LabTest.all.each do |lab_test|
     #   @visit_lab_test_xref = @visit.visit_lab_test_xrefs.build(visit_id: @visit.id, lab_test_id: lab_test.id)
-    end
   end
   def edit
   end

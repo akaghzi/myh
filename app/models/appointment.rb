7ab78a8aa01e183765a8df93df9613004a3074ab
Asim Kaghzi <akaghzi@gmail.com>
2013-06-14 01:19:22 -0600

    add appointment date validation

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,10 @@ class Appointment < ActiveRecord::Base
   belongs_to :user
   belongs_to :appointment_slot
   validates :user_id, :patient_id, :appointment_date, :appointment_slot_id, :appointment_reason, presence: true
-  validates :appointment_slot_id, uniqueness: {scope: :appointment_date}
+  validates :appointment_slot_id, uniqueness: {scope: [:patient, :appointment_date], message: "can exist once for a patient"}
+  validate :check_date
   scope :todays, ->{where("appointment_date >= ?", Date.today)}
+  def check_date
+      self.errors.add(:appointment_date, "is invalid") if appointment_date < Date.today 
+  end
 end

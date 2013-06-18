f0f9058720006dae54ad416f7f285abad11b7e83
Asim Kaghzi <akaghzi@gmail.com>
2013-06-18 00:36:18 -0600

    upgraded to rc2

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,8 +6,8 @@ class Appointment < ActiveRecord::Base
   validates :user_id, :patient_id, :appointment_date, :appointment_slot_id, :appointment_reason, presence: true
   validates :appointment_slot_id, uniqueness: {scope: [:patient, :appointment_date], message: "can exist once for a patient"}
   validate :check_date
-  scope :future, ->{where("appointment_date >= ?", Date.today)}
-  scope :todays, ->{where("appointment_date >= '#{Date.yesterday}' and appointment_date <= '#{Date.tomorrow}'")}
+  scope :future, ->{where("appointment_date > ?", Date.today)}
+  scope :todays, ->{where("appointment_date > '#{Date.yesterday}' and appointment_date < '#{Date.tomorrow}'")}
   def check_date
       self.errors.add(:appointment_date, "is invalid") if appointment_date < Date.today 
   end
